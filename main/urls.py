05601e088b1c88ec7e376ae156fa1b9060d84dbf
Big Pootis <timurizhdan@gmail.com>
2023-12-05 14:43:30 +0100

    initial commit

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  
-    path('hot', views.hot)
+    path('', views.index, name='home'),  
+    path('login', views.login, name='log')
 ]
