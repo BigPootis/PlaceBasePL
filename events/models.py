63f213567a33d23a52a64c5c1bb4948751327c93
Big Pootis <timurizhdan@gmail.com>
2023-12-06 16:44:12 +0100

    initial commit

--- events/models.py
+++ events/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+
+
+
+
 class Articles(models.Model):
     title=models.CharField('Название', max_length=75 )
     anons=models.CharField('Анонс', max_length= 250)
     full_text=models.TextField('Статья')
     date=models.DateTimeField('Дата публикации')
-    img=models.ImageField(upload_to='files/events')
+    img=models.ImageField(upload_to='events/files/events', verbose_name=True)
+
 
     def __str__(self):
         return self.title
